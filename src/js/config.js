76fd3f181f22e3e0c29d77fdc5a9feb395566551
mpomento <mpomento@gmail.com>
2023-06-28 11:11:34 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6c2e794c-7e08-44cb-b24d-d774b79a82da'
+export const MODAL_CLOSE_SEC = 2.5;
